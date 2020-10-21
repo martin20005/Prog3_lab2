05f152d44b3bfedd8a87e000126899125c4f4ccf
Martin Ronai-Kovacs <martin.bkg@gmail.com>
2020-10-21 10:37:02 +0200

    7. kész; sol/feladatok_4 és sol/feladatok_7 csomagolásra készek.

---
+++ sol/feladat_4/CaesarFrame.java
@@ -0,0 +1,61 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.*;
+
+public class CaesarFrame extends JFrame 
+{
+	Object[] abc = new Object[] {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+	
+	private JTextField inputText = new JTextField(20);
+	private JTextField outputText = new JTextField(20);
+	private JButton codeButton = new JButton("Code!");
+	private JComboBox<?> offsetCombo = new JComboBox<>(abc);
+	
+	class EncodeActionListener implements ActionListener
+	{		
+		public void actionPerformed(ActionEvent ae)
+		{
+			if (ae.getActionCommand().contentEquals("encode"))
+			{
+				Caesar c = new Caesar();
+				outputText.setText(
+						(new Caesar()).getCaesarCode(
+						inputText.getText(), 
+						(char) offsetCombo.getSelectedItem()
+						));
+			}
+		}
+	}
+	
+	public CaesarFrame() 
+	{
+		super("SwingLab");
+		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		//this.setName("SwingLab");
+		this.setSize(400, 110);
+		this.setResizable(true);
+		
+		// layout
+		BoxLayout layout = new BoxLayout(this.getContentPane(), BoxLayout.Y_AXIS);
+		getContentPane().setLayout(layout);
+
+		// panelek és feltöltésük
+		JPanel upper = new JPanel();
+		JPanel lower = new JPanel();
+		
+		upper.add(offsetCombo);
+		upper.add(inputText);
+		upper.add(codeButton);
+		lower.add(new JLabel("Output:"));
+		lower.add(outputText);
+
+		// panelek jozzáadása a frame-hez
+		this.add(upper);
+		this.add(lower);
+		
+		outputText.setEditable(false);
+		codeButton.setActionCommand("encode");
+		codeButton.addActionListener(new EncodeActionListener());
+	}
+}
---
+++ sol/feladat_7/Main.java
@@ -0,0 +1,19 @@
+
+public class Main {
+
+	public static void main(String[] args)
+	{
+		// 1
+		/*
+		Caesar c = new Caesar();
+		java.util.Scanner sc = new java.util.Scanner(System.in);
+		System.out.println(c.getCaesarCode(sc.next(), 'B'));
+		sc.close();
+		*/
+		
+		// 2
+		CaesarFrame cf = new CaesarFrame();
+		cf.setVisible(true);
+	}
+
+}
